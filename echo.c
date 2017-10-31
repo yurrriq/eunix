ca181ad6f62703ab6ec88106ddb9c76cdcad9e31
Eric Bailey <eric@ericb.me>
2017-10-31 14:11:54 -0400

    echo: handle -n and start writing prose

--- echo.c
+++ echo.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
+#include <getopt.h>
 
 
 void usage();
 
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-  usage();
-  return 0;
+    int newline = 1;
+
+    int c;
+
+    while ((c = getopt(argc, argv, "n")) != EOF) {
+        switch (c) {
+        case 'n':
+            newline = 0;
+            break;
+        case '?':
+            usage();
+            return 1;
+        }
+    }
+
+    int index;
+
+    for (index = optind; index < argc; index++) {
+        printf("%s", argv[index]);
+        if (index < argc - 1)
+            putchar(' ');
+    }
+
+    if (newline)
+        putchar('\n');
+
+    return 0;
 }
 
 
 void usage()
 {
-  printf("\n"
-         "       echo -- display a line of text"
-         "\n");
+    printf("Usage: echo [-n] [string ...]\n");
 }
